eabdd1118601e7b0af02ecff90a159359236e0c1
Jack Bracey <jack.bracey@humboldt.co.uk>
2018-09-19 09:02:08 +0100

    Improved Randoms class

--- src/com/humboldtsltns/calcengine/Randoms.java
+++ src/com/humboldtsltns/calcengine/Randoms.java
@@ -2,45 +2,37 @@ package com.humboldtsltns.calcengine;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Randoms {
 
-    private Scanner scanner;
     ArrayList<String> names = new ArrayList<String>();
     ArrayList<String> surnames = new ArrayList<String>();
     ArrayList<String> letter = new ArrayList<String>();
 
+    public Randoms() {
+
+    }
+
     //Gens
     public String nameGen() {
 
-        loadFile("forenames.txt");
-
-        while(scanner.hasNext()) {
-            names.add(scanner.next());
-        }
+        loadFile("forenames.txt", names);
 
         return names.get(ranNumGen(names.size()-1));
     }
 
     public String surnameGen() {
 
-        loadFile("surnames.txt");
-
-        while(scanner.hasNext()) {
-            surnames.add(scanner.next());
-        }
+        loadFile("surnames.txt", surnames);
 
         return names.get(ranNumGen(names.size()-1));
     }
 
     public String letterGen() {
 
-        loadFile("alphabet.txt");
-
-        while(scanner.hasNext()) {
-            letter.add(scanner.next());
-        }
+        loadFile("alphabet.txt", letter);
 
         return letter.get(ranNumGen(letter.size()-1));
     }
@@ -53,14 +45,17 @@ public class Randoms {
         return (int)(Math.random() * maxNum + 1);
     }
 
-
-
-    private void loadFile(String fileName) {
+    private void loadFile(String fileName, List<String> list) {
+        Scanner scanner;
         try{
             scanner = new Scanner(new File(fileName));
+            while(scanner.hasNext()) {
+                list.add(scanner.next());
+            }
         } catch(Exception e) {
             System.out.println(e + ": Could not find file");
         }
+
     }
 
 }
